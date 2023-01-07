4c84a12a2f47ec22353f294681701b83de367596
lIlDirE <47134949+lIlDirE@users.noreply.github.com>
2023-01-07 12:50:27 +0200

    sec commit

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,29 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+
+const listEl = document.querySelector('.gallery');
+
+const createImgEL = galleryItems
+.map(image => `<div class="gallery__item" id="openModalbBtn"> <a class="gallery__link" href="${image.original}"> <img class="gallery__image" src="${image.preview}" data-source="${image.original}" alt="${image.description}"/></a></div>`)
+.join('');
+
+listEl.insertAdjacentHTML('beforeend', createImgEL);
+
+document.body.addEventListener("click", (e) => {
+	e.preventDefault()
+})
+
+const container = document.querySelector(`.js-container`);
+
+container.addEventListener('click', onClick);
+
+function onClick(evt){
+	if (evt.target.nodeName !== `IMG`){
+		return;
+	}
+		const instance = basicLightbox.create(`
+		<img src="${evt.target.dataset.source}" width="800" height="600">
+		`)
+
+		instance.show();
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,20 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const listEl = document.querySelector('.gallery');
+
+const createImgEL = galleryItems
+.map(({preview, original, description}) => {
+	return `	
+		<a class="gallery__link" href="${original}">
+		<img class="gallery__image" src="${preview}" alt="${description}"/>
+		</a>
+	`;}).join('');
+
+listEl.insertAdjacentHTML('beforeend', createImgEL);
+
+var lightbox = new SimpleLightbox('.gallery a', { 
+	captions: true,
+	captionsData: "alt",
+	captionsDelay: 250
+	});
